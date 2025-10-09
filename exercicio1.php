d53d3ff36a7aadb75fefe2280ae84b5b157b6d7f
ggJeans <Siqjean1331@gmail.com>
2025-10-08 23:38:53 -0300

    first commit

---
+++ exercicio1.php
@@ -0,0 +1,22 @@
+<?php 
+
+/*
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: Flores
+Turma: ESOFT-2B
+Componentes:
+ 25004000-1 - Ana Castela da Silva
+ 25004001-2 - Pedro Alvares Cabral
+ 25004001-3 - Roberto Carlos de Andrade
+ 25004001-4 - Sergio Roberto Gularte
+ 25004001-5 - Lauana Prado de Andrade
+Data: 08 de Outubro de 2025
+Descritivo: Escreva um programa que use for para imprimir os números de 1 a 10.
+*/
+
+for ($i = 1; $i <= 10; $i++) {
+    echo $i . " ";
+}
+
+?>
