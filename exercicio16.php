f6e671173b3ea836b6934354392a196907b2bc2a
ggJeans <Siqjean1331@gmail.com>
2025-10-21 19:48:49 -0300

    Update exercicio16.php

--- exercicio16.php
+++ exercicio16.php
@@ -6,6 +6,7 @@ Disciplina: Linguagem e Técnicas de Programacão
 Professor: Flores
 Turma: ESOFT-2B
 Componentes:
+25178060-2 - Rodrigo Samuel Gulka
 25363636-2 - Jheymis Bueno Gonçalves
 25363611-2 - Thiago de Barros Inácio
 25363487-2 - Leonardo Schimock Silveira
@@ -21,6 +22,7 @@ echo "Cidades:\n";
 foreach ($cidades as $cidade) {
     echo $cidade . PHP_EOL;
 }
+
 
 
 ?>
