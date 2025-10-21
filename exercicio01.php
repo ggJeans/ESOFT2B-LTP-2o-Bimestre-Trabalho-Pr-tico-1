7ec5e20e2fe94a184d0c4a638669cd072a00e9ca
ggJeans <Siqjean1331@gmail.com>
2025-10-21 19:45:32 -0300

    Update student ID for Rodrigo Samuel Gulka

--- exercicio01.php
+++ exercicio01.php
@@ -6,7 +6,8 @@ Disciplina: Linguagem e Técnicas de Programacão
 Professor: Flores
 Turma: ESOFT-2B
 Componentes:
- 25363636-2 - Jheymis Bueno Gonçalves
+25178060-2 - Rodrigo Samuel Gulka
+25363636-2 - Jheymis Bueno Gonçalves
 25363611-2 - Thiago de Barros Inácio
 25363487-2 - Leonardo Schimock Silveira
 25357718-2 - Jean Carlos Siqueira dos Santos
