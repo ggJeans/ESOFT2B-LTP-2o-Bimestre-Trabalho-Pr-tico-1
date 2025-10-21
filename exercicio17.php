de76d76a2a5bc23c6f59f88f76f8b9091ddc199d
ggJeans <Siqjean1331@gmail.com>
2025-10-21 19:49:03 -0300

    Update student names in exercicio17.php

--- exercicio17.php
+++ exercicio17.php
@@ -8,6 +8,7 @@ Disciplina: Linguagem e Técnicas de Programacão
 Professor: Flores
 Turma: ESOFT-2B
 Componentes:
+            25178060-2 - Rodrigo Samuel Gulka
             25363636-2 - Jheymis Bueno Gonçalves
             25363611-2 - Thiago de Barros Inácio
             25363487-2 - Leonardo Schimock Silveira
@@ -22,6 +23,7 @@ $numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 //echo da metade do array:
 echo $numeros [4]; 
 //50
+
 
 
 ?>
