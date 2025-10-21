df01873e7056432b239196a5519269dcd73c43a3
ggJeans <Siqjean1331@gmail.com>
2025-10-21 19:47:15 -0300

    Refactor comments and maintain functionality

--- exercicio08.php
+++ exercicio08.php
@@ -6,6 +6,7 @@ Disciplina: Linguagem e Técnicas de Programacão
 Professor: Flores
 Turma: ESOFT-2B
 Componentes:
+25178060-2 - Rodrigo Samuel Gulka
 25363636-2 - Jheymis Bueno Gonçalves
 25363611-2 - Thiago de Barros Inácio
 25363487-2 - Leonardo Schimock Silveira
@@ -21,3 +22,4 @@ while ($i <= 30) {
 }
 
 ?>
+
