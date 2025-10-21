efe535e3dd1da933a59af12a20133cdf4afc41c0
ggJeans <Siqjean1331@gmail.com>
2025-10-21 19:46:39 -0300

    Modify student details in exercicio05.php
    Updated student names in the comments section.

--- exercicio05.php
+++ exercicio05.php
@@ -6,6 +6,7 @@ Disciplina: Linguagem e Técnicas de Programacão
 Professor: Flores
 Turma: ESOFT-2B
 Componentes:
+25178060-2 - Rodrigo Samuel Gulka
 25363636-2 - Jheymis Bueno Gonçalves
 25363611-2 - Thiago de Barros Inácio
 25363487-2 - Leonardo Schimock Silveira
@@ -22,3 +23,4 @@ for ($i = 1; $i <= 100; $i++) {
 
 echo "A soma dos números de 1 a 100 é: " . $soma;
 ?>
+
